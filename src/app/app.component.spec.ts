d085db5cdc7926be18e7beaeeaaf6793d9340a92
wilson.junior <wilson.junior@saude.rj.gov.br>
2024-03-04 14:45:44 -0300

    Alterando texto front

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, agendacontatos');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Olá Wilson, Agenda Contatos');
   });
 });
